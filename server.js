fdd54fd322ab4ef676686a9b1737063943787968
Patricia <englishwithpatricia007@gmail.com>
2025-04-28 09:49:28 -0300

    metodos atualizados para usar o prisma

--- server.js
+++ server.js
@@ -6,46 +6,54 @@ const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
 
-const users = [];
 
-
-app.get('/usuarios', (req, res) => {
-    res.status(200).json(users)
-}
-);
-
-app.post('/usuarios', (req, res) => {
-    users.push(req.body)
+app.post('/usuarios', async (req, res) => {
+    await prisma.user.create ({
+        data: {
+            name: req.body.name,
+            email: req.body.email,        
+        }
+    })    
     res.status(201).json(req.body);
 });
+
+app.get('/usuarios', async (req, res) => {
+    const users = await prisma.user.findMany()
+    res.status(200).json(users);
+}); 
+
+
+app.put('/usuarios/:id', async (req, res) => {
+   await prisma.user.update ({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            name: req.body.name,
+            email: req.body.email,        
+        }
+    })    
+    res.status(201).json(req.body);
+});
+
+app.delete('/usuarios/:id', async (req, res) => {
+    await prisma.user.delete ({
+         where: {
+             id: req.params.id
+         }
+     })    
+     res.status(200).json({message: 'Usuario deletado com sucesso!'});
+ });
+
 
 app.listen(3333, () => {
     console.log('Servidor rodando na porta 3333');
 })
 
 
-/*
-Criar nossa API de usuários
+//1) método HTTP: GET, POST, PUT, DELETE, PATCH, OPTIONS, HEAD
+//2) URL: /api/v1/usuarios
 
-1) Criar um servidor HTTP com Express   
-2) Criar uma rota GET /usuarios que retorne um JSON com a mensagem "GET /usuarios"
-3) Criar uma rota GET /usuarios que retorne um texto "GET /usuarios"
-
-4) Criar um usuário
-5) Criar um usuário com o método POST /usuarios que retorne um JSON com a mensagem "POST /usuarios"
-
-6) Listar todos os usuários com o método GET /usuarios que retorne um JSON com a mensagem "GET /usuarios"
-7) Listar um usuário com o método GET /usuarios/:id que retorne um JSON com a mensagem "GET /usuarios/:id"
-
-8) Editar um usuário com o método PUT /usuarios/:id que retorne um JSON com a mensagem "PUT /usuarios/:id"
-9) Deletar um usuário com o método DELETE /usuarios/:id que retorne um JSON com a mensagem "DELETE /usuarios/:id"
-*/
-
-
-/*
-1) método HTTP: GET, POST, PUT, DELETE, PATCH, OPTIONS, HEAD
-2) URL: /api/v1/usuarios
-*/
 
 // Dados sensíveis: senhas, tokens, dados pessoais Body Params
 // Dados não sensíveis: nome, email, telefone, endereço Query Params / Route Params
